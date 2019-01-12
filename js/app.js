6cc84de70b3a2467782e4f556630d00c906de13d
Hiram <hiram@upscalemedia.mx>
2019-01-11 18:36:07 -0600

    JS ejercicios

--- js/app.js
+++ js/app.js
@@ -78,10 +78,12 @@ function multiplica() {
 
 // ---------------
 
-var numero = prompt("Introduce un número y se mostrará su factorial");
-var resultado = 1;
+// var numero = prompt("Introduce un número y se mostrará su factorial");
+// var resultado = 1;
 
-for(var i=1; i<=numero; i++) {
-  resultado *= i;
-}
-alert(resultado);
+// for(var i=1; i<=numero; i++) {
+//   resultado *= i;
+// }
+// alert(resultado);
+
+// ---------------
