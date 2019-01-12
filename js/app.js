c7dfc2caa98f0032b739b459c10448cae8eb3f62
Hiram <hiram@upscalemedia.mx>
2019-01-11 18:55:46 -0600

    JS ejercicios

--- js/app.js
+++ js/app.js
@@ -87,3 +87,27 @@ function multiplica() {
 // alert(resultado);
 
 // ---------------
+
+/** Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento. A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas. */
+
+function info(cadena) {
+
+  var resultado = 'La cadena "' + cadena + '"';
+
+  // Comprobar mayúsculas y minúsculas
+  if (cadena == cadena.toUpperCase()) {
+    resultado += " está formada sólo por mayúsculas";
+  } else if (cadena == cadena.toLowerCase()) {
+    resultado += " está formada sólo por minúsculas";
+  } else {
+    resultado += " está formada por mayúsculas y minúsculas";
+  }
+
+  return resultado;
+}
+document.getElementById('mensaje1').innerHTML = info("OVNI = OBJETO VOLADOR NO IDENTIFICADO");
+document.getElementById('mensaje2').innerHTML = info("En un lugar de la mancha...");
+document.getElementById('mensaje3').innerHTML = info("...no hay papel....");
+
+
+// ---------------
