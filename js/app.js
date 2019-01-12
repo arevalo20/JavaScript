e7c076b4c7f4ea3c4c3ec9a26b9f3626bca8401c
Hiram <hiram@upscalemedia.mx>
2019-01-11 18:57:29 -0600

    JS ejercicios

--- js/app.js
+++ js/app.js
@@ -111,3 +111,5 @@ document.getElementById('mensaje3').innerHTML = info("...no hay papel....");
 
 
 // ---------------
+
+/** Definir una función que determine si la cadena de texto que se le pasa como parámetro es un palíndromo, es decir, si se lee de la misma forma desde la izquierda y desde la derecha. Ejemplo de palíndromo complejo: "La ruta nos aporto otro paso natural". */
